113dc537b965a8b02d91b492447acb13c95c10c7
Bruno Lara Tavares <bruno.exz@gmail.com>
2010-12-14 00:09:32 -0200

    Adicionado README ingles/portugues. Nome das variavies e funções renomeados para o ingles

--- pgnator.php
+++ pgnator.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Pgnator
- * @version 0.3
+ * @version 0.5
  */
 /*
 Plugin Name: Pgnator
@@ -14,16 +14,16 @@ Author URI: #
 class Pgnator
 {
 //Deck
-public $numPosts = 1;
-public $urlLimpo = true;
+public $numPosts = 8;
+public $cleanURL = true;
 private $total;
 private $pg = 1;
 private $numpgs;
-private $prox = "&raquo;";
-private $ant = "&laquo;";
-private $ult = "&gt;";
-private $pri = "&lt";
-private $faixa = 2;
+private $next = "&raquo;";
+private $prev = "&laquo;";
+private $last = "&gt;";
+private $first = "&lt";
+private $range = 2;
 
 function Pgnator($css = "default"){
     //css
@@ -31,7 +31,7 @@ function Pgnator($css = "default"){
     echo '<link type="text/css" rel="stylesheet" href="'. $css .'" />';
 }
 
-function conteudo($query, $sql = false)
+function content($query, $sql = false)
 {
 
 $this->pg = is_null($_GET["pg"]) ? 1 : $_GET["pg"];
@@ -50,35 +50,39 @@ $this->pg = is_null($_GET["pg"]) ? 1 : $_GET["pg"];
     
     $this->numpgs = $this->total / $this->numPosts;
     //retorna query dinamica
+    //dinamic query return
 
     return $content;
 }
 
 
-function criarMenu()
+function createMenu()
 {
-    $endereco = $this->urlLimpo ? $_SERVER["REDIRECT_URL"] . "?pg=" : "?page_id=".$_GET[page_id]."&pg=";
+    $address = $this->cleanURL ? $_SERVER["REDIRECT_URL"] . "?pg=" : "?page_id=".$_GET[page_id]."&pg=";
     $str = "<ul class='pgnator'>";
        if ($this->numpgs > 1)
        {
            //Link para pagina anterior, se for preciso
+           //Link for previous page, if necessary
             if($this->pg > 1){
-                $str .= "<li><a title='Primeiro' href='".$endereco."1'>".$this->pri."</a></li>";
-                $str .= "<li><a title='Anterior' href='".$endereco.($this->pg-1)."'>".$this->ant."</a></li>";
+                $str .= "<li><a title='Primeiro' href='".$address."1'>".$this->first."</a></li>";
+                $str .= "<li><a title='Anterior' href='".$address.($this->pg-1)."'>".$this->prev."</a></li>";
             }
                  
             //Loop para números baseado na faixa
+            //Number loop based on range
             for ($cont = 1; $cont <= ceil($this->numpgs); $cont ++)
             {
                 $class = $cont == $this->pg ? 'active' : '';
-                if(($cont < $this->pg + $this->faixa) && ($cont > $this->pg - $this->faixa))
-                $str .= "<li><a class='".$class."' href='".$endereco.$cont."'>".$cont."</a></li>";
+                if(($cont < $this->pg + $this->range) && ($cont > $this->pg - $this->range))
+                $str .= "<li><a class='".$class."' href='".$address.$cont."'>".$cont."</a></li>";
             }
-            //Link para proxima pagina, se for preciso
+            //Link para nextima pagina, se for preciso
+            //Link for the last page, if necessary
             if($this->pg < $this->numpgs )
             {
-                $str .= "<li><a title='Próximo' href='".$endereco.($this->pg+1)."'>".$this->prox."</a></li>";
-                $str .= "<li><a title='Ultimo' href='".$endereco.ceil($this->numpgs)."'>".$this->ult."</a></li>";
+                $str .= "<li><a title='Próximo' href='".$address.($this->pg+1)."'>".$this->next."</a></li>";
+                $str .= "<li><a title='Ultimo' href='".$address.ceil($this->numpgs)."'>".$this->last."</a></li>";
             }
                 
        }
@@ -87,18 +91,21 @@ function criarMenu()
 }
 
 
-function renomear($ant,$prox)
+function rename($previous,$next)
 {
     //Renomeia os textos dos link de proximo e anterior
- $this->ant = $ant;
- $this->prox = $prox;
+    //Rename the next and previous link
+ $this->prev = $previous;
+ $this->next = $next;
 }
 
-function mudarFaixa($int)
+function changeRange($int)
 {
     //Muda faixa de numero
- $this->faixa = $int + 1;
+    //Change numbers range
+ $this->range = $int + 1;
 }
+
 }
 
 ?>
